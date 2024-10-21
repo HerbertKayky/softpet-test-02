4159c68ac09eaa0bbcc87be7d3e59e9a70b90192
HerbertKayky <herbertkayky9@gmail.com>
2024-10-21 18:52:47 -0300

    adicionado <Link> para /Home  ao ícone e nome softpet do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import Container from "../container";
 import { FaRegUser } from "react-icons/fa";
 import { signOut, useSession } from "next-auth/react";
 import { FiLogOut } from "react-icons/fi";
-import Link from "next/link"; 
+import Link from "next/link";
 
 export function Header() {
   const { data: session } = useSession();
@@ -14,8 +14,11 @@ export function Header() {
     <Container>
       <header className="flex items-center py-10 justify-between">
         <div className="flex items-center gap-2">
-          <Image src={logoimg} alt="Logo softmakers" width={60} />
-          <h1 className="text-white font-medium text-3xl">SoftPet</h1>
+          <Link href="/" className="flex gap-2 items-center">
+            <Image src={logoimg} alt="Logo softmakers" width={60} />
+            <h1 className="text-white font-medium text-3xl">SoftPet</h1>
+          </Link>
+
           <h1 className="text-white text-xl">oii {session?.user?.email}</h1>
         </div>
 
