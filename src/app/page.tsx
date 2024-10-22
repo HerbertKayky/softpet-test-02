3012be68238cb52ac8967ad336e1df42753ec7f3
HerbertKayky <herbertkayky9@gmail.com>
2024-10-22 20:10:54 -0300

    criado componente Search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Search } from "@/components/search";
 import { Header } from "../components/header";
 
 export default function Home() {
   return (
     <div className="bg-gradient-dark-blue min-h-screen">
-      
+      <Search />
     </div>
   );
 }
