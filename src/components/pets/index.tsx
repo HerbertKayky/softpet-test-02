bdc5bbd90badfeb6c48fa0c66a5f8aeed8e0edd5
HerbertKayky <herbertkayky9@gmail.com>
2024-10-26 18:15:48 -0300

    melhora o estilo CSS da seção de Pets e implementa verificação de autenticação para cadastro de pets, permitindo apenas usuários logados.

--- src/components/pets/index.tsx
+++ src/components/pets/index.tsx
@@ -4,10 +4,16 @@ import { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { CiSearch } from "react-icons/ci";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { MdOutlinePets } from "react-icons/md";
+import { FaEdit, FaPhoneVolume, FaRegTrashAlt } from "react-icons/fa";
+import { IoIosArrowDown, IoIosArrowUp, IoMdCalendar } from "react-icons/io";
+import { PiDna } from "react-icons/pi";
+import { LuUserCircle2 } from "react-icons/lu";
+
 import { api } from "@/lib/api";
 import Container from "../container";
 import { PetProps } from "@/utils/pet.type";
+import { useSession } from "next-auth/react";
 
 type FormValues = {
   name: string;
@@ -19,6 +25,7 @@ type FormValues = {
 };
 
 export default function PetsSearch() {
+  const { data: session } = useSession();
   const { register, handleSubmit, reset } = useForm<FormValues>();
   const [pets, setPets] = useState<PetProps[]>([]);
   const [openModal, setOpenModal] = useState<number | null>(null);
@@ -63,6 +70,30 @@ export default function PetsSearch() {
     }
   };
 
+  function calculateAge(birthDate: Date): number {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDifference = today.getMonth() - birth.getMonth();
+
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && today.getDate() < birth.getDate())
+    ) {
+      age--;
+    }
+
+    return age;
+  }
+
+  const handleOpenModal = () => {
+    if (session?.user) {
+      setIsModalOpen(true);
+    } else {
+      alert("Login necessário");
+    }
+  };
+
   return (
     <Container>
       {/* Search e Botão de cadastro */}
@@ -89,13 +120,12 @@ export default function PetsSearch() {
         </div>
         <button
           className="flex items-center gap-1 px-3 py-2 bg-gradient-blue text-white rounded-md font-bold ml-4"
-          onClick={() => setIsModalOpen(true)}
+          onClick={handleOpenModal}
         >
           <AiOutlinePlusCircle size={28} color="#FFF" />
           Cadastrar
         </button>
       </div>
-
 
       {/* Modal de cadastro de Pet */}
       {isModalOpen && (
@@ -167,47 +197,74 @@ export default function PetsSearch() {
         </div>
       )}
 
-
       {/* Pet List */}
-      <main className="w-full max-w-7xl mx-auto px-2">
-        <ul className="flex flex-wrap gap-4">
+      <main className="w-full mx-auto px-2">
+        <ul className="flex flex-wrap gap-4 justify-center">
           {pets.map((pet) => (
             <li
               key={pet.id}
-              className="relative border rounded-lg p-4 bg-[#0c1224] shadow-lg flex-1 min-w-[250px] max-w-[300px]"
+              className={`relative flex items-center w-full max-w-[275px] rounded-md py-3 my-1 ${
+                openModal === pet.id ? "outline-2 outline-blue-500" : ""
+              } bg-gradient-dark-blue outline-none hover:outline-2 hover:outline-blue-500 transition-all`}
             >
-              <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-4">
-                  <div className="w-12 h-12 rounded-full bg-blue-500 flex items-center justify-center">
-                    {pet.pet === "cat" ? (
-                      <img src="/cat.svg" alt="Cat" className="w-8 h-8" />
-                    ) : (
-                      <img src="/dog.svg" alt="Dog" className="w-8 h-8" />
-                    )}
+              <div className="rounded-full p-3 mx-2 bg-gradient-blue">
+                <img
+                  src={pet.pet === "cat" ? "/cat.svg" : "/dog.svg"}
+                  alt="ícone do animal"
+                  className="w-10 h-10"
+                />
+              </div>
+
+              <div className="flex justify-between flex-1">
+                <div>
+                  <div className="flex items-center gap-2">
+                    <MdOutlinePets size={22} color="#FFF" />
+                    <p className="text-white text-lg">{pet.name}</p>
                   </div>
-                  <div>
-                    <h2 className="text-xl text-white">{pet.name}</h2>
-                    <p className="text-gray-400">{pet.ownerName}</p>
+
+                  <div className="flex items-center gap-2">
+                    <LuUserCircle2 size={22} color="#FFF" />
+                    <p className="text-white text-lg">{pet.ownerName}</p>
                   </div>
                 </div>
-                <button
-                  className="text-white"
-                  onClick={() => toggleModal(pet.id)}
-                >
+
+                <button className="mr-2" onClick={() => toggleModal(pet.id)}>
                   {openModal === pet.id ? (
-                    <IoIosArrowUp size={24} />
+                    <IoIosArrowUp size={33} color="#FFF" />
                   ) : (
-                    <IoIosArrowDown size={24} />
+                    <IoIosArrowDown size={33} color="#FFF" />
                   )}
                 </button>
               </div>
 
               {openModal === pet.id && (
-                <div className="absolute top-full left-0 mt-2 w-full bg-gray-800 p-4 rounded-lg shadow-md z-10">
-                  <p>Raça: {pet.race}</p>
-                  <p>Tipo: {pet.pet}</p>
-                  <p>Telefone: {pet.phone}</p>
-                  <p>Nascimento: {new Date(pet.birth).toLocaleDateString()}</p>
+                <div className="absolute z-10 left-0 top-full mt-3 w-full rounded-md p-4 bg-gradient-dark-blue outline outline-blue-500">
+                  <div className="flex items-center gap-1 mb-2">
+                    <PiDna size={20} color="#FFF" />
+                    <p className="text-white">Raça: {pet.race}</p>
+                  </div>
+                  <div className="flex items-center gap-1 mb-2">
+                    <FaPhoneVolume size={20} color="#FFF" />
+                    <p className="text-white">Telefone: {pet.phone}</p>
+                  </div>
+                  <div className="flex items-center gap-1 mb-4">
+                    <IoMdCalendar size={20} color="#FFF" />
+                    <p className="text-white">
+                      Idade: {calculateAge(new Date(pet.birth))} Anos (
+                      {new Date(pet.birth).toLocaleDateString("pt-BR")})
+                    </p>
+                  </div>
+
+                  <div className="flex flex-col gap-2">
+                    <button className="bg-white px-4 py-2 rounded-md text-blue-600 font-bold flex items-center justify-center gap-2">
+                      <FaEdit size={20} />
+                      Editar
+                    </button>
+                    <button className="bg-gradient-blue px-4 py-2 rounded-md text-white font-bold flex items-center justify-center gap-2">
+                      <FaRegTrashAlt size={20} />
+                      Remover
+                    </button>
+                  </div>
                 </div>
               )}
             </li>
