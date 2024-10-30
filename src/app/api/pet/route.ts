fa7de9e4716fcd35f76e752249a79f07998656b7
HerbertKayky <herbertkayky9@gmail.com>
2024-10-29 23:09:58 -0300

    cria rota PUT para pets

--- src/app/api/pet/route.ts
+++ src/app/api/pet/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
   try {
     const userId = session?.user.id ? Number(session.user.id) : undefined;
-    
+
     if (!userId) {
       return NextResponse.json("User not authenticated", { status: 401 });
     }
@@ -58,6 +58,49 @@ export async function GET(request: Request) {
     return NextResponse.json(
       { error: "Falha ao buscar pets" },
       { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request: Request) {
+  const { id, name, ownerName, phone, pet, race, birth } = await request.json();
+  const session = await getServerSession(authOptions);
+
+  try {
+    const userId = session?.user.id ? Number(session.user.id) : undefined;
+
+    if (!userId) {
+      return NextResponse.json("User not authenticated", { status: 401 });
+    }
+
+    // Verifica se o pet existe e pertence ao usuário
+    const existingPet = await prismaClient.pet.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!existingPet) {
+      return NextResponse.json({ error: "Pet not found" }, { status: 404 });
+    }
+
+    // Atualiza o pet
+    await prismaClient.pet.update({
+      where: { id: Number(id) },
+      data: {
+        name,
+        ownerName,
+        phone,
+        pet,
+        race,
+        birth: new Date(birth),
+      },
+    });
+
+    return NextResponse.json({ message: "Pet updated successfully" });
+  } catch (err) {
+    console.error("Erro no servidor", err);
+    return NextResponse.json(
+      { error: "Failed to update pet" },
+      { status: 400 }
     );
   }
 }
