3a49cacc80d988bf3f92ceaf286ac65f52f60363
HerbertKayky <herbertkayky9@gmail.com>
2024-11-06 21:19:34 -0300

    adiciona layout.tsx à página /dashboard para alterar o título do site

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "SoftPet | Dashboard",
+};
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
