c1aa97d28b4bef7996c9f68b1d7e736f364497ed
HerbertKayky <herbertkayky9@gmail.com>
2025-01-13 23:12:39 -0300

    Atualiza nome do site quando está no /perfil

---
+++ src/app/perfil/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "SoftPet | Perfil",
+};
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
